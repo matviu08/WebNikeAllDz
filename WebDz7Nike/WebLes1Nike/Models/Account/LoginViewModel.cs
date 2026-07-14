6106c8d9e84b267329e90fb36c929377f3cacf31
matviu08 <matviu08@gmail.com>
2026-07-14 21:31:39 +0300

    dz7

---
+++ WebDz7Nike/WebLes1Nike/Models/Account/LoginViewModel.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WebLes1Nike.Models.Account
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Необхідно вказати Email")]
+        [EmailAddress(ErrorMessage = "Некоректний формат Email")]
+        [Display(Name = "Email")]
+        public string Email { get; set; } = null!;
+        [Required(ErrorMessage = "Необхідно вказати пароль")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Пароль має містити щонайменше {2} символів")]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; } = null!;
+        [Display(Name = "Запам'ятати мене")]
+        public bool RememberMe { get; set; }
+        public string? ReturnUrl { get; set; }
+    }
+}
