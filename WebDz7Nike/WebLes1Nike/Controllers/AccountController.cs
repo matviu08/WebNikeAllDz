171748b5c1b646eb721ef2d39da700df4ba77735
matviu08 <matviu08@gmail.com>
2026-07-16 20:46:37 +0300

    les8

--- WebDz7Nike/WebLes1Nike/Controllers/AccountController.cs
+++ WebDz7Nike/WebLes1Nike/Controllers/AccountController.cs
@@ -1,9 +1,13 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using WebLes1Nike.Constants;
+using WebLes1Nike.Data.Entities.Identity;
+using WebLes1Nike.Interfaces;
 using WebLes1Nike.Models.Account;
 
 namespace WebLes1Nike.Controllers
 {
-    public class AccountController : Controller
+    public class AccountController(UserManager<UserEntity> userManager, IImageService imageService, SignInManager<UserEntity> signInManager) : Controller
     {
         [HttpGet]
         public IActionResult Login(string? returnUrl = null)
@@ -13,16 +17,24 @@ namespace WebLes1Nike.Controllers
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Login(LoginViewModel model)
+        public async Task<IActionResult> Login(LoginViewModel model)
         {
             if (!ModelState.IsValid)
             {
                 return View(model);
             }
 
-            
-            
-            TempData["Message"] = "Форма входу валідна (візуальна демонстрація).";
+            var user = await userManager.FindByEmailAsync(model.Email);
+            if (user != null)
+            {
+                var result = await userManager.CheckPasswordAsync(user, model.Password);
+                if (result)
+                {
+                    await signInManager.SignInAsync(user, false);
+                    return Redirect(model.ReturnUrl ?? "/");
+                }
+            }
+            ModelState.AddModelError(String.Empty, "Дані вказано не вірно");
             return RedirectToAction(nameof(Login));
         }
         [HttpGet]
@@ -32,17 +44,55 @@ namespace WebLes1Nike.Controllers
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Register(RegisterViewModel model)
+        public async Task<IActionResult> Register(RegisterViewModel model)
         {
             if (!ModelState.IsValid)
             {
                 return View(model);
             }
-
+            var user = await userManager.FindByEmailAsync(model.Email);
+            if (user != null)
+            {
+                ModelState.AddModelError("Email", "дана пошта уже зареєстрованна");
+                return View(model);
+            }
+            user = new UserEntity()
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Email = model.Email,
+                UserName = model.Email,
+                Image = "default.jpg"
+            };
+            if (model.Image != null)
+            {
+                string folderPath = Path.Combine(Directory.GetCurrentDirectory() ,"wwwroot", "images");
+                var fileName = await imageService.SaveOptimizedImageAsync(model.Image, folderPath);
+                user.Image = fileName;
+            }
             
+            var result = await userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                await userManager.AddToRoleAsync(user, Roles.User);
+                await signInManager.SignInAsync(user, false);
+                return Redirect("/");
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return View(model);
+        }
 
-            TempData["Message"] = "Форма реєстрації валідна (візуальна демонстрація).";
-            return RedirectToAction(nameof(Register));
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return Redirect("/");
         }
     }
 }
